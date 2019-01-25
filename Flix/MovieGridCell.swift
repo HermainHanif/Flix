468325de5b1cbbe1cbe0dd60a4788d32c5351ed8
Hamzah <hermainhanif@hermains-macbook-air.local>
2019-01-25 06:29:46 -0800

    I implemented the basic requirements for part 2, without the orientation which will be added later

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Hermain Hanif on 1/25/19.
+//  Copyright © 2019 Hermain Hanif. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
